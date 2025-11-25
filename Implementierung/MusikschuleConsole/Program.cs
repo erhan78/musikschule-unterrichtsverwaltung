1d0d2965c421136ce2272bfd50879662e2062721
erhan78 <de.erhan@gmx.de>
2025-11-25 19:56:12 +0100

    Initialer Commit der Konsolen Anwendung

---
+++ Implementierung/MusikschuleConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MusikschuleConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
