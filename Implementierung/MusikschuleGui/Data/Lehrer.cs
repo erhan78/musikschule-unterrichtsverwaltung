db304d804f86c11e3b612ab491e072435f9af502
erhan.dereli_1 <erhan.dereli_1@042SPC1012037>
2026-02-02 09:49:37 +0100

    EFCore: Lehrer implementierung

---
+++ Implementierung/MusikschuleGui/Data/Lehrer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusikschuleGui.Data
+{
+    internal class Lehrer
+    {
+        public int LehrerId { get; set; }
+        public string Vorname { get; set; } = "";
+        public string Nachname { get; set; } = "";
+        public string Fachgebiet { get; set; } = "";
+        public string? Email { get; set; }
+        public string? Telefon { get; set; }
+
+        //Unterricht stunde
+    }
+}
