6620bf9345df306398e64d9b4c0430b67a50e1be
erhan.dereli_1 <erhan.dereli_1@042SPC1012037>
2026-02-02 09:47:53 +0100

    EFCore: Schueler implementierung

---
+++ Implementierung/MusikschuleGui/Data/Schueler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusikschuleGui.Data
+{
+    public class Schueler
+    {
+        public int SchuelerId { get; set; }
+        public string Vorname { get; set; } = "";
+        public string Nachname { get; set; } = "";
+        public string Instrument { get; set; } = "";
+        public string? Email { get; set; }
+        public string? Telefon { get; set; }
+        
+        //Unterrichts stunden
+    }
+}
